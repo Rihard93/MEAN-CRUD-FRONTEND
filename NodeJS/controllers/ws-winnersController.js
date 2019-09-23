1bb481be7ab475e1484461e084bde7351a5965ac
Ricardo Meza <ricardo.93meza@gmail.com>
2019-09-23 15:27:44 -0600

    Busqueda por ID
    Se agrego el metodo de busqueda por ID.

--- NodeJS/controllers/ws-winnersController.js
+++ NodeJS/controllers/ws-winnersController.js
@@ -2,11 +2,24 @@
 const express = require("express"); //Libreria para poder hacer uso del framework express.js
 var router = express.Router();
 var { WS_Winner } = require("../models/ws-winners");
+var ObjectId = require('mongoose').Types.ObjectId; //Variable para realizar la busqueda de datos por medio de ID's validos
 
 //Se obtiene toda la informacion almacenada en la base de datos
 router.get('/', (req, res) => {
     WS_Winner.find((err,docs) => {
         if (!err) { res.send(docs); }
+        else { console.log("ERROR: Couldn't retrive data from database :" + JSON.stringify(err,undefined,2));  }
+    });
+});
+
+//Se obtiene la informacion por medio del id
+router.get('/:id',(req,res) =>{
+    if(!ObjectId.isValid(req.params.id))        
+        return res.status(400).send(`No information found with the provided id : ${req.params.id}`);
+    
+    //Si el ID es correcto se realizar la busqueda por medio del ID enviado
+    WS_Winner.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
         else { console.log("ERROR: Couldn't retrive data from database :" + JSON.stringify(err,undefined,2));  }
     });
 });
